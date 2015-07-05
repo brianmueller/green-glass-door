2b3e04fcf5553863000d6bca790ca4b3f3656aea
brianmueller <brianscottmueller@gmail.com>
2015-07-05 12:55:39 +0000

    controller setup

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,23 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  
+#   get '/stage1' do
+#     erb :stage1
+#   end
+  
+  post '/stage1' do
+    # ruby code
+    @user_input = params[:user_input]
+    @user_can = ggd_can(@user_input)
+    @user_color = ggd_color(@user_input)
+    erb :stage1
   end
 
 end
