467e13aba59d8b6e7fd50dd2b282ed60ac4c65b0
brianmueller <brianscottmueller@gmail.com>
2016-06-23 09:12:21 -0400

    fix twilio API

--- models/texter.rb
+++ models/texter.rb
@@ -1,14 +1,14 @@
 def send_message(queries)
   to_phone = ("+1" + to_phone.to_s)
-  
-  account_sid = 'AC57d30f6f3e6ef74916066b9cd7bac481'
-  auth_token = '226214696c13297f828dfa269879c6d8'
+
+  account_sid = 'ACed3ed813257f8acedfce46a695216257'
+  auth_token = 'cb1dd832eda91ea39319fe6827f1650b'
 
   # set up a client to talk to the Twilio REST API
   @client = Twilio::REST::Client.new account_sid, auth_token
 
   @client.messages.create(
-    from: '+16467591817',
+    from: '+14342605034',
     to: '+17209512343',
     body: "You have #{queries} queries remaining."
   )
